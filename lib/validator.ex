74f65c6278deac9a5e75313a15b3d07a31d5a289
misaelpc <misaelpcgm@gmail.com>
2015-03-19 09:00:14 -0600

    Review Schema validations

--- lib/validator.ex
+++ lib/validator.ex
@@ -4,7 +4,7 @@ defmodule Reception.Validator do
   #####
   # External API
   def start_link() do
-    file_xsd = '/Users/hiphoox/Development/Elixir/testbed/recepcion/BalanzaComprobacion_1_1.xsd'
+    file_xsd = './BalanzaComprobacion_1_1.xsd'
     {_ok, xsd} = :xmerl_xsd.process_schema(file_xsd)
     GenServer.start_link( __MODULE__, xsd, name: __MODULE__)
   end
@@ -18,9 +18,20 @@ defmodule Reception.Validator do
 
   def handle_call({:validate, xml}, _from, xsd) do 
       case :xmerl_xsd.validate(xml, xsd) do 
-        {:error,_message} -> { :reply, {:error, "Esto salio mal"}, xsd}
+        {:error, message} ->
+          #IO.inspect :xmerl_xsd.format_error(message)
+          parse_errors(message,xsd)
+          { :reply, {:error, "Esto salio mal"}, xsd}
         {_xml,_newState} ->   { :reply, {:ok}, xsd}
       end
+  end
+
+  def parse_errors([],_), do: []
+
+  def parse_errors([head | tail],xml) do
+    IO.puts "***********************"
+    IO.inspect :xmerl_xsd.format_error({xml,head})
+    parse_errors(tail,xml)
   end
 
 end
--- web/controllers/validator_controller.ex
+++ web/controllers/validator_controller.ex
@@ -7,18 +7,22 @@ defmodule Reception.ValidatorController do
 
     {:ok, document, _conn_details} = Plug.Conn.read_body(conn)
     request_body = :erlang.bitstring_to_list(document)
-    
+
     try do
       {xml,_} = :xmerl_scan.string(request_body)
 
       case Reception.Validator.validate(xml) do 
-        {:error,message} -> text conn, "XML no cumple con la addenda"  
-        {:ok} ->  Reception.Secure.save(xml,request_body)
-                  text conn, "XML Valid"     
+        {:error,message} -> 
+          text conn, "XML no cumple con la addenda"  
+        {:ok} ->  
+          #Reception.Secure.save(xml,request_body)
+          text conn, "XML Valid"     
       end
 
     catch
-      :exit, _ -> text conn, "XML INVALIDO"
+      :exit, _ ->
+        IO.inspect request_body
+        text conn, "XML INVALIDO"
     end
 
   end
